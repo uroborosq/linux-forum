4b7604c15a95ccacaeae2ae6e1566d175ec35436
Dmitriy Tikhomirov <uroborosq@ya.ru>
2023-02-08 00:12:44 +0300

    feat: created template project

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as process from "process";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn']
+  });
+  let port = process.env.PORT;
+  if (port == "") {
+    port = "3000"
+    console.log("WARN: $PORT is not specified, using 3000 by default")
+  }
+  await app.listen(parseInt(port));
+}
+bootstrap();
